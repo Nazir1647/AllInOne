7898081b29a1ca90da033d514c632d34c754e8b3
Nazir1647 <naziransari.tlh@gmail.com>
2024-01-14 15:57:16 +0530

    Call by javascript create and delete

--- AllInOne/Areas/Popup/Controllers/PopupController.cs
+++ AllInOne/Areas/Popup/Controllers/PopupController.cs
@@ -20,17 +20,55 @@ namespace AllInOne.Areas.Popup.Controllers
             return View(data);
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Save = "Save";
+            EmployeePopup emp = new EmployeePopup();
+            return PartialView("_EmployeeModalPartial", emp);
+        }
+
         [HttpPost]
         public IActionResult Create(EmployeePopup form)
         {
-            if (ModelState.IsValid)
+            if(form.Id == 0)
             {
                 _context.employeePopups.Add(form);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
+                TempData["success"] = "Record saved successfully";
             }
-            return PartialView("_EmployeeModalPartial", form);
+            else
+            {
+                _context.employeePopups.Update(form);
+                TempData["success"] = "Record updated successfully";
+            }          
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            var data = _context.employeePopups.FirstOrDefault(x => x.Id == Id);
+            if(data == null)
+            {
+                return Json(false);
+            }
+            ViewBag.Save = "Update";
+            return PartialView("_EmployeeModalPartial", data);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var data = _context.employeePopups.Find(Id);
+            if(data == null)
+            {
+                return NotFound();
+            }
+            _context.employeePopups.Remove(data);
+            _context.SaveChanges();
+            TempData["success"] = "Record deleted successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
--- AllInOne/wwwroot/js/Popup.js
+++ AllInOne/wwwroot/js/Popup.js
@@ -3,10 +3,31 @@
     $('button[data-toggle="ajax-modal"]').click(function (event) {
 
         var url = $(this).data('url');
+        var type = $(this).data('type');
         var decodeUrl = decodeURIComponent(url);
         $.get(decodeUrl).done(function (data) {
             placeHolderElement.html(data);
             placeHolderElement.find('.modal').modal('show');
+        });
+    });
+
+    placeHolderElement.on('click', '[data-save="modal"]', function (event) {
+        var form = $(this).parents('.modal').find('form');
+        var actionurl = form.attr('action');
+        var url = "/popup/popup/" + actionurl;
+        var sendData = form.serialize();
+        $.post(url, sendData).done(function (data) {
+            placeHolderElement.find('.modal').modal('hide');
+        });
+    });
+
+
+    $('button[data-toggle="ajax-delete"]').click(function (event) {
+
+        var url = $(this).data('url');
+        var decodeUrl = decodeURIComponent(url);
+        $.get(decodeUrl).done(function (data) {
+            alert("Deleted");
         });
     });
 
